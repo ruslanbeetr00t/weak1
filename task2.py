f0029ee608650961374375436d91e0c53def7f16
пк <treshmetal.ua@gmail.com>
2021-05-18 17:38:19 +0300

    init

---
+++ task2.py
@@ -0,0 +1,17 @@
+# hello beetroot
+print('hello beetroot')
+
+#massege1 s.t.a.l.k.e.r
+print('s','t','a','l','k','e','r', sep='.',end='\n')
+#massege2 x-16 waiting
+print('x-16 waiting', end= '\n')
+#massege3 need to hack door use python bugagaga
+print('need to hack door use python bugagaga',end='\n')
+
+#pleas help me understand, how to work python
+print('help me',end=' ')
+print('understand ,',end=' ')
+print('how to work python')
+
+# одне прям дужу-дуже-дуже довге речення для виконання printа з максимальною кількістю символів в одному рядку,ще
+print('одне прям дужу-дуже-дуже довге речення для виконання printа з максимальною кількістю символів в одному рядку,ще')
